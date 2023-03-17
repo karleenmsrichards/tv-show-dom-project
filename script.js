02aea05665f3a8a06819a2be3c2eea87740e6c65
Karleen Richards <karleenmsrichards@gmail.com>
2023-03-17 08:01:55 +0000

    did some stretch goals

--- script.js
+++ script.js
@@ -1,8 +1,123 @@
-//You can edit ALL of the code here
-
-let allEpisodes = [];
+// //You can edit ALL of the code here
 
 const allShows = getAllShows();
+
+// ///////Slideshow
+
+const mostPopularShows = allShows.filter((obj) => obj.rating.average >= 9);
+
+// const imagesForSlideshow = mostPopularShows.map((item) => {
+//   // console.log(item.rating.average);
+//   // console.log(mostPopularShows.length);
+//   // const slideshowImage = document.createElement("img");
+//   // slideshowImage.classList.add("slideshow-image");
+//   // const slideshowWrapper = document.getElementById("slideshow-wrapper");
+//   // slideshowWrapper.appendChild(slideshowImage);
+//   // slideshowImage.src =
+//   return (
+//     item.image?.original ??
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSweXgcvq_bb0THeLZJ0daDBuSgxMWpQ5BTw8r0_6B0&s"
+//   );
+// });
+
+// const radioButtons = document.querySelectorAll('input[name="sort-by"]');
+// btn.addEventListener("click", () => {
+//   let selectedSize;
+//   radioButtons.forEach((radioButton) => {
+//     if (radioButton.checked) {
+//       selectedSize = radioButton.value;
+//     }
+//   });
+//   console.log(selectedSize);
+// });
+let sortBy = ""
+const radioButtons = document.querySelectorAll('input[name="sort-by"]');
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("click", (e) => {
+    console.log(e.target.value);
+    sortBy = e.target.value
+    myFileter(e.target.value)
+  });
+});
+
+const imagesForSlideshow = mostPopularShows.map((item) => {
+  console.log(item.rating.average);
+  console.log(mostPopularShows.length);
+  const slideshowImage = document.createElement("img");
+  slideshowImage.classList.add("slideshow-image");
+  const slideshowWrapper = document.getElementById("slideshow-wrapper");
+  console.log(slideshowWrapper);
+  slideshowWrapper.appendChild(slideshowImage);
+  slideshowImage.src =
+    item.image?.original ??
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSweXgcvq_bb0THeLZJ0daDBuSgxMWpQ5BTw8r0_6B0&s";
+});
+
+// console.log(imagesForSlideshow);
+
+// let count = 2;
+// let intervalId = null;
+
+// const slideshowWrapper = document.querySelector(".slideshow-wrapper");
+
+// function dragging(e) {
+//   slideshowWrapper.scrollLeft = e.pageX;
+// }
+// slideshowWrapper.addEventListener("load", () => {
+//   setIntervalId = setInterval(() => {}, 3000);
+// });
+
+// window.addEventListener("load", () => {
+//   intervalId = setInterval(goForward, 3000);
+// });
+
+// function goForward() {
+//   const slideshowImage = document.getElementById("slideshow-wrapper");
+//   slideshowImage.style.border = "2px dotted red";
+//   slideshowImage.style.height = "500px";
+//   count++;
+//   slideshowImage.style.backgroundImage = `url(${imagesForSlideshow[count]})`;
+
+//   if (count === imagesForSlideshow.length) {
+//     count = 0;
+//     slideshowImage.style.backgroundImage = imagesForSlideshow[count];
+//   }
+// }
+
+// window.addEventListener("load", () => {
+//   intervalId = setInterval(goBackward, 3000);
+// });
+
+// function goForward() {
+//   count++;
+//   let showSlideshow = document.getElementById("show-slideshow");
+//   let urlS = allShows.map((item) => item.image.medium);
+
+//     showSlideshow.src =
+//       item.image?.medium ??
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSweXgcvq_bb0THeLZJ0daDBuSgxMWpQ5BTw8r0_6B0&s";
+
+//     item[count];
+//     if (count === allShows.length) {
+//       count = 0;
+//       showSlideshow.src = item[count];
+//     }
+//   );
+// }
+
+// function goBackward() {
+//   if (count === 0) {
+//     count = imagesArray.length;
+//     imgSlideshow.src = imagesArray[count];
+//   }
+
+//   count--;
+//   imgSlideshow.src = imagesArray[count];
+// }
+
+//////
+
+let allEpisodes = [];
 
 const sortedShows = allShows.sort((a, b) => {
   let fa = a.name.toLowerCase(),
@@ -17,9 +132,17 @@ const sortedShows = allShows.sort((a, b) => {
   return 0;
 });
 
+function truncateSummary(summary) {
+  return `${summary.substr(0, 250)}...`;
+}
+
 function pageForShows(sortedShows) {
   let root = document.getElementById("root");
   root.innerHTML = "";
+
+  let totalShows = document.querySelector(".search-results");
+  totalShows.innerText = `Displaying ${sortedShows.length} shows`;
+  // totalShows.style.fontStyle = "italic";
 
   let showSectionContainter = document.createElement("div");
   showSectionContainter.setAttribute("id", "show-section-container");
@@ -43,13 +166,33 @@ function pageForShows(sortedShows) {
       let showId = item.id;
       fetchAllEpsiodes(showId);
       // showsSelect.style.display = "none";
-
+      let newTitle = document.getElementById("main-title");
+      newTitle.innerText = item.name;
       setUpEpisodeSearch();
+      displayingShowsLength(sortedShows);
     });
 
     let showSummary = document.createElement("p");
     showSummary.classList.add("show-summary");
-    showSummary.innerHTML = item.summary;
+    showSummary.innerHTML =
+      item.summary.length <= 250 ? item.summary : truncateSummary(item.summary);
+
+    ////
+
+    let readMoreSummary = document.createElement("a");
+    readMoreSummary.innerText =
+      showSummary.innerHTML === item.summary ? "" : "Read more";
+    let showShortSummary = true;
+
+    readMoreSummary.addEventListener("click", () => {
+      showShortSummary = !showShortSummary;
+      showSummary.innerHTML = showShortSummary
+        ? truncateSummary(item.summary)
+        : item.summary;
+      showSummary.appendChild(readMoreSummary);
+    });
+
+    /////
 
     let showGroup = document.createElement("div");
     showGroup.classList.add("show-group");
@@ -79,6 +222,7 @@ function pageForShows(sortedShows) {
     showSectionContainter.appendChild(showSection);
     showSection.appendChild(showImage);
     showSection.appendChild(showSummary);
+    showSummary.appendChild(readMoreSummary);
     showSection.appendChild(showGroup);
 
     showGroup.appendChild(showRating);
@@ -107,7 +251,19 @@ function showsSelect(shows) {
 
   showsSelect.addEventListener("change", (event) => {
     const showId = event.target.value;
+    let mainTitle = document.getElementById("main-title");
+    let filteredShow = sortedShows.filter(
+      (show) => Number(show.id) === Number(showId)
+    );
+    console.log(showId);
+    console.log(event.target.value);
+    let newTitle = filteredShow.map((item) => item.name);
+    console.log(newTitle);
+    mainTitle.innerText = `/ ${newTitle}`;
+
     fetchAllEpsiodes(showId);
+    fetchCasts(showId);
+
     setUpEpisodeSearch();
   });
 }
@@ -137,7 +293,7 @@ function setUpShowSearch() {
 
 function displayingShowsLength(shows) {
   let display = document.querySelector(".search-results");
-  display.innerText = `Displaying ${shows.length} / ${shows.length} show(s)`;
+  display.innerText = `Found ${shows.length} show(s)`;
 }
 
 function displayingAllEpisodesLength(episodes) {
@@ -148,20 +304,45 @@ function displayingAllEpisodesLength(episodes) {
 function pageForEpisodes(episodes) {
   const rootElem = document.getElementById("root");
   rootElem.innerHTML = "";
+  console.log(sortedShows);
 
-  const displayAllShowsLink = document.createElement("button");
-  displayAllShowsLink.classList.add("display-all-shows-link");
-  displayAllShowsLink.innerText = "All Shows";
-  rootElem.appendChild(displayAllShowsLink);
-  console.log(displayAllShowsLink.innerText);
-  displayAllShowsLink.addEventListener("click", pageForShows);
+  let displayAllShowsLink = document.getElementById("all-shows-link");
+  console.log(displayAllShowsLink);
+  displayAllShowsLink.addEventListener("click", () => {
+    let newTitle = document.getElementById("main-title");
+    newTitle.innerText = "Shows";
+    let display = document.querySelector(".search-results");
+    display.innerText = "";
+    displayAllShowsLink.style.display = "none";
+    pageForShows(sortedShows);
+    let episodeSearchWrapper = document.getElementById(
+      "episode-search-wrapper"
+    );
+    episodeSearchWrapper.style.display = "none";
+    let episodeSelectWrapper = document.getElementById(
+      "episode-select-wrapper"
+    );
+
+    episodeSelectWrapper.style.display = "none";
+  });
+
+  let display = document.querySelector(".search-results");
+  display.style.display = "block";
+
+  displayAllShowsLink.style.display = "inline";
+
+  let episodeSearchWrapper = document.getElementById("episode-search-wrapper");
+  episodeSearchWrapper.style.display = "inline";
+
+  let episodeSelectWrapper = document.getElementById("episode-select-wrapper");
+  episodeSelectWrapper.style.display = "inline";
 
   let episodeSectionContainer = document.createElement("div");
   episodeSectionContainer.classList.add("episode-section-container");
 
   let page = episodes.forEach((item) => {
     let newDiv = document.createElement("div");
-    newDiv.classList.add("new-div");
+    newDiv.classList.add("episode-section");
 
     let image = document.createElement("img");
     image.classList.add("image");
@@ -170,7 +351,7 @@ function pageForEpisodes(episodes) {
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSweXgcvq_bb0THeLZJ0daDBuSgxMWpQ5BTw8r0_6B0&s";
 
     let heading = document.createElement("h2");
-    heading.classList.add("heading");
+    heading.classList.add("episode-title");
 
     let name = item.name;
     let season = item.season.toString().padStart(2, "0");
@@ -196,6 +377,9 @@ function pageForEpisodes(episodes) {
     rootElem.appendChild(episodeSectionContainer);
   });
   displayingAllEpisodesLength(episodes);
+  let cast = document.createElement("button");
+  cast.innerText = "Cast";
+  cast.addEventListener("click", fetchCasts);
 }
 
 function menu() {
@@ -205,14 +389,6 @@ function menu() {
     mobileMenuItems.classList.toggle("open");
   });
 }
-
-//LEVEL 200 ////////////////////////
-///////////////ADD LIVE SEARCH INPUT
-/////////////////1.ONLY DISPLAY EPISODES WHO'S NAMES AND SUMMARY ARE INCLUDED IN THE SEARCH AREA
-/////////////////////2. SEARCH SHOULD BE CASE-INSENSITIVE
-////////////////////////////3.DISPLAY SHOULD UPDATE IMMEDIATELY
-/////////////////////////////////4. DISPLAY HOW MANY EPISODES MATCH CURRENT SEARCH
-///////////////////////////////////////5. IF SEARCH AREA IS EMPTY DISPLAY ALL EPISODES
 
 function filteredSearch() {
   let searchInput = document.getElementById("episode-input");
@@ -233,15 +409,6 @@ function filteredSearch() {
   displayingAllEpisodesLength(filteredArr);
 }
 
-// function setUpEpisodeSearch() {
-//   const episodeSearch = document.getElementById("episode-search-wrapper");
-//   episodeSearch.style.display = "block";
-//   const showSearch = document.getElementById("show-search-wrapper");
-//   showSearch.style.display = "none";
-//   let episodeInput = document.getElementById("episode-input");
-//   episodeInput.addEventListener("keyup", filteredSearch);
-// }
-
 function setUpEpisodeSearch() {
   let episodeInput = document.getElementById("episode-input");
   episodeInput.addEventListener("keyup", filteredSearch);
@@ -252,15 +419,9 @@ function displayingAllEpisodesLength(episodes) {
   display.innerText = `Displaying ${episodes.length} / ${episodes.length} episode(s)`;
 }
 
-//LEVEL 300 ////////
-///////ADD SELECT TO JUMP TO SELECTED EPISODE
-//////////1.EPISODES TO BE LISTED IN FORMAT S01E05
-//////////////2.ON SELECTION USER SHOULD BE TAKEN DIRECTLY TO EPISODE
-//////////////////3.SHOW THE SELECTED EPISODE ONLY OR CREATE A LINK TO EPISODE
-/////////////////////4.MAKE A WAY FOR THE USER TO GET BACK TO ALL EPISODES
-
 function populateEpisodesSelect(episodes) {
   let episodeSelect = document.getElementById("episode-select");
+  episodeSelect.innerHTML = "";
 
   episodes.forEach((item) => {
     let optionField = document.createElement("option");
@@ -273,8 +434,8 @@ function populateEpisodesSelect(episodes) {
     episodeSelect.appendChild(optionField);
   });
 
-  let formContainer = document.querySelector(".form");
-  formContainer.appendChild(episodeSelect);
+  // let formContainer = document.querySelector(".form");
+  // formContainer.appendChild(episodeSelect);
 
   episodeSelect.addEventListener("change", (event) => {
     console.log(event.target.value);
@@ -284,12 +445,10 @@ function populateEpisodesSelect(episodes) {
       let season = item.season.toString().padStart(2, "0");
       let episode = item.number.toString().padStart(2, "0");
       let designation1 = `#S${season}E${episode} - ${name}`;
-
       return designation1 === designation;
     });
 
     pageForEpisodes(filteredEpisode);
-
     displayingAllEpisodesLength(filteredEpisode);
     // filter episode by creating a same page link below////
     // let newDivLocation = event.target.value;
@@ -297,39 +456,73 @@ function populateEpisodesSelect(episodes) {
   });
 }
 
-//LEVEL 350///////////
-///////////////PAGE SHOULD LOAD WITH THE SAME EPISODES BUT FETCH FROM API
-//////////////////1.YOUR SEARCH AN EPISODE SELECTOR MUST CONTINUE TO WORK AS BEFORE
-////////////////////////2. DO NOT RE-FETCH EPISODES EACH TIME A USER TYPES A CHARACTER
-
-//LEVEL 400///////////
-
 const fetchAllEpsiodes = async (showId) => {
   // url: https://api.tvmaze.com/shows/SHOW_ID/episodes
   const res = await fetch(`https://api.tvmaze.com/shows/${showId}/episodes`);
   allEpisodes = await res.json();
-
+  console.log(allEpisodes);
   pageForEpisodes(allEpisodes);
   populateEpisodesSelect(allEpisodes);
 };
 
-// function setup() {
-//   // let allShows = getAllShows();
-//   const episodeSearch = document.getElementById("episode-search-wrapper");
-//   episodeSearch.style.display = "none";
-//   const showSearch = document.getElementById("show-search-wrapper");
-//   showSearch.style.display = "block";
+const fetchCasts = async (showId) => {
+  // url: https://api.tvmaze.com/shows/SHOW_ID/episodes
+  const res = await fetch(`http://api.tvmaze.com/shows/${showId}?embed=cast`);
+  list = await res.json();
+  console.log(list);
 
-//   pageForShows(sortedShows);
-//   showsSelect(sortedShows);
-//   setUpShowSearch();
-// }
+  let castArray = list._embedded.cast;
+  let castContainer = document.createElement("div");
+  castContainer.classList.add("cast-container");
+
+  let detailsElement = document.createElement("details");
+  let summary = document.createElement("summary");
+  summary.innerText = "Casts";
+  detailsElement.appendChild(summary);
+  let castText = document.createElement("h2");
+  let root = document.getElementById("root");
+  castText.innerText = "Casts";
+  root.appendChild(castText);
+  castArray.forEach((obj) => {
+    let section = document.createElement("div");
+
+    let castMemberImage = document.createElement("img");
+    castMemberImage.classList.add("cast-member-image");
+    castMemberImage.src = obj.person.image.medium;
+
+    let castMemberName = document.createElement("p");
+    castMemberName.classList.add("cast-member-name");
+    castMemberName.innerText = obj.person.name;
+
+    section.appendChild(castMemberImage);
+    section.appendChild(castMemberName);
+
+    castContainer.appendChild(section);
+  });
+
+  root.insertBefore(detailsElement, root.children[0]);
+  detailsElement.appendChild(castContainer);
+};
 
 function setup() {
-  // let allShows = getAllShows();
   pageForShows(sortedShows);
   showsSelect(sortedShows);
   setUpShowSearch();
+  const clickBtn = document.getElementById("click-btn");
+  const ratingRadioButtons = document.querySelectorAll("input[sort-by]");
+  for (const btn of ratingRadioButtons) {
+    btn.addEventListener("change", () => {
+      console.log("radio");
+      pageForShows(allShows);
+    });
+  }
 }
+
+// const filterRating = () => {
+//   const rating = doucment.getElementById("rating");
+//   rating.addEventListener("checked", () => {
+//     pageForShows(allShows);
+//   });
+// };
 
 window.onload = setup;
